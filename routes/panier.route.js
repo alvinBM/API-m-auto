9a681578a10f67e314868f3d43210fc2bb59d171
davmaene <davidmened@gmail.com>
2021-07-02 18:54:32 +0200

    up route edit panier

--- routes/panier.route.js
+++ routes/panier.route.js
@@ -3,8 +3,9 @@ import panier from '../controllers/panier.controller';
 
 const router = express.Router();
 
-router.post('/', panier.create); // Creation d'un panier
-router.get('/:panierId', panier.findOne);// Details d'un panier
+router.post('/', panier.create)// Creation d'un panier
+      .get('/:panierId', panier.findOne) // Details d'un panier
+      .put('/:panierId', panier.edit)
     
 
 export default router;
