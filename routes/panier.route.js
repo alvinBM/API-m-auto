f64f3a97458f87cef0a45e9e3c4a8d412f23416e
adkif <akifungo@gmail.com>
2021-07-02 14:40:52 +0200

    #12 route to create panier has implemented

--- routes/panier.route.js
+++ routes/panier.route.js
@@ -3,6 +3,7 @@ import panier from '../controllers/panier.controller';
 
 const router = express.Router();
 
+router.post('/', panier.create); // Creation d'un panier
 router.get('/:panierId', panier.findOne);// Details d'un panier
     
 
