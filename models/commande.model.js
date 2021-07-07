7bf3a7eeebdf7ad3bf7af0bb79da6f1c173f5dae
adkif <akifungo@gmail.com>
2021-07-07 10:32:41 +0200

    #18 Modify Sql tables commandes and paniers with defaults values

--- models/commande.model.js
+++ models/commande.model.js
@@ -10,26 +10,15 @@ const Commande = db.define('commandes',{
     },
     created: {
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)'),
-        allowNull: false,
-        field: created
     },
     modified: {
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP'),
-        allowNull: false,
-        field: modified
     },
     deleted: {
         type: Sequelize.DATE,
-        allowNull: true,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP'),
-        field: deleted
     },
     status: {
         type: Sequelize.INTEGER,
-        allowNull: false,
-        defaultValue: 1
     },
     client_id:{
         type: Sequelize.INTEGER,
--- models/panier.model.js
+++ models/panier.model.js
@@ -10,26 +10,15 @@ const Panier = db.define('paniers',{
     },
     created: {
         type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)'),
-        field: created
     },
     modified: {
         type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP'),
-        field: modified
     },
     deleted: {
         type: Sequelize.DATE,
-        allowNull: true,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP'),
-        field: deleted
     },
     status: {
         type: Sequelize.INTEGER,
-        allowNull: false,
-        defaultValue: 1
     },
     client_id:{
         type: Sequelize.INTEGER,
