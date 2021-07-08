6cfc39fed37db666e52208a89c9d1bfb45fd5215
Jean Zelote <mulondajean94@hotmail.com>
2021-07-08 13:26:02 +0200

    commit tache pour MULONDA Jean

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,57 +3,51 @@ import produits from '../models/produits';
 import dotenv from 'dotenv';
 import base64ToImage from 'base64-to-image';
 import path from "path";
+import { Op } from 'sequelize';
+import Categories from '../models/categories';
 
 dotenv.config();
 
 const productController = {
 
-    listerProduits : async (req, res) => {
 
-        let results = await produits.findAll({
-            where: {
-                status: 1
-            }
-        }).then((data) => {
-            res.status(200).json({
-                status : "200",
-                "produits" : data
-            })
-        }).catch(er => console.error(er));
+    modifierProduits: async(req, res) => {
 
-    },
+        Produits.hasMany(Categories, { foreignKey: "id" });
+        Categories.belongsTo(Produit, { foreignKey: "produits_id" });
 
-
-    rechercherProduits : async (req, res) => {
-
-        let query = req.body.query;
-
-        if(query){
-            let results = await produits.findAll({
+        await Produits.findOne({
                 where: {
-                    status: 1,
-                    nom : req.body.query
-                }
-            }).then((data) => {
-                res.status(200).json({
-                    status : "200",
-                    "produits" : data
-                })
-            }).catch(er => console.error(er));
-        }else{
-            res.status(401).json({
-                status : "401",
-                "desciption" : "Vous devez renseigner le mot clé de la recherche"
+                    id: req.params.ProduitsId,
+                    categories_id: req.body.categories_id,
+                },
+                include: [{
+                    model: Produits,
+                    required: true,
+                    as: 'Produits'
+                }, ],
             })
-        }
-
-        
-
+            .then(mproduits => {
+                if (mproduits && mproduits instanceof Produit) {
+                    mproduits = req.body
+                    mproduits.save()
+                    res
+                        .status(200)
+                        .json({ status: 200, message: "Produit modifié avec succès !" })
+                } else {
+                    res
+                        .status(404)
+                        .json({ status: 404, message: "aucun resultat trouvé pour la modification !" })
+                }
+            })
+            .catch(error => {
+                res
+                    .status(500)
+                    .json({ status: 500, message: error.hasOwnProperty('sqlMessage') ? error['sqlMessage'] : "erreur inconnue du serveur !" })
+            })
     }
 
-
-}
+};
 
 
 export default productController;
-
