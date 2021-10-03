41371ed4265b5652706e589a733c3bf2498ac967
davmaene <davidmened@gmail.com>
2021-10-03 21:04:51 +0200

    up line

--- middlewares/tokenValidator.js
+++ middlewares/tokenValidator.js
@@ -1,5 +1,10 @@
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const validateToken = async (req, res, next) => {
-
+    const _publicToken = req.headers['token'];
+    await jwt.verify( _publicToken, process.env.expire_token, (rejected, resolved) => {
+        
+    })
 }
