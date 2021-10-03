99fca36982bc607b9f6e22bbe19a559dff2997c6
davmaene <davidmened@gmail.com>
2021-10-03 20:31:56 +0200

    up line

--- routes/client.route.js
+++ routes/client.route.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import clients from '../controllers/client.controller';
-
+import validateKey from '../middlewares/validateKey';
 const router = express.Router();
 
 router
-    .get('/', clients.onListing)
-    .put('/:clientId', clients.onEditing)
-    .post('/register', clients.register)
-    .post('/login', clients.login)
+    .get('/', validateKey,clients.onListing)
+    .put('/:clientId', validateKey,clients.onEditing)
+    .post('/register', validateKey,clients.register)
+    .post('/login', validateKey,clients.login)
     
 export default router;
